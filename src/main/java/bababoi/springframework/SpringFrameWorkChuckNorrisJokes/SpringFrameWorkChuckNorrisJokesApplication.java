c6cb926d37ebf36a32bdbaae08914502c4336ab1
TheBababoi <georgekamidis@gmail.com>
2022-12-04 12:26:17 +0200

    creation of spring boot project. closes#1

---
+++ src/main/java/bababoi/springframework/SpringFrameWorkChuckNorrisJokes/SpringFrameWorkChuckNorrisJokesApplication.java
@@ -0,0 +1,13 @@
+package bababoi.springframework.SpringFrameWorkChuckNorrisJokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameWorkChuckNorrisJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameWorkChuckNorrisJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bababoi/springframework/SpringFrameWorkChuckNorrisJokes/SpringFrameWorkChuckNorrisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package bababoi.springframework.SpringFrameWorkChuckNorrisJokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameWorkChuckNorrisJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
